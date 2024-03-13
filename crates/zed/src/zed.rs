8890a231db16b34204f30653b1434591f2db96a4
Richard McHorgh <mchorghr2@gmail.com>
2024-03-13 02:08:20 -0400

    fixed test

--- crates/zed/src/zed.rs
+++ crates/zed/src/zed.rs
@@ -2976,7 +2976,7 @@ mod tests {
             language::init(cx);
             editor::init(cx);
             project_panel::init_settings(cx);
-            collab_ui::init(&app_state, cx);
+            collab_ui::init(cx);
             project_panel::init((), cx);
             terminal_view::init(cx);
             assistant::init(cx);
